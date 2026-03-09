d6a3250369915f71787bb8b78cc16fb9f14e5c70
cyberspider217 <47828975+cyberspider217@users.noreply.github.com>
2026-03-09 11:02:21 -0400

    Add files via upload

--- RMBD/service.sh
+++ RMBD/service.sh
@@ -9,10 +9,10 @@ sleep 10
 # disable Wi-Fi multicast
 dumpsys wifi disable-multicast
 
+# disable using a more permanent option
+ip link set wlan0 multicast off
+
 # disable Nearby Share
 settings put global nearby_sharing_enabled 0
 settings put secure nearby_sharing_slice_enabled 0
 
-# force LTE only (SIM 1 & SIM 2)
-settings put global preferred_network_mode 11
-settings put global preferred_network_mode1 11
